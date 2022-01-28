59af1031d6b742cce6abc0f5c202549e50fc0d2f
FlierK254 <flierpeter9@gmail.com>
2022-01-28 16:23:55 +0300

    Javascript DOM concept

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+ let element = document.querySelectorAll("#primary .link");
+ for (var i=0;i<=element.length ;i+=2) {
+
+        element[i].style.backgroundColor="black"
+        element[i].style.color="red";
+     } 
+    
+    
